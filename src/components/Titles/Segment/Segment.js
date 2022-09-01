c8adc7015bdb028a9efe143ed1a6d73819f18f7c
cryndry <ismailkaraagac94@gmail.com>
2022-09-01 22:24:40 +0300

    Movies added

--- src/components/Titles/Segment/Segment.js
+++ src/components/Titles/Segment/Segment.js
@@ -1,7 +1,4 @@
 import s from './Segment.module.css'
-import thumbnail10h from '../../../assets/thumbnail-top10-h.jpg'
-import thumbnail10 from '../../../assets/thumbnail-top10.jpg'
-import thumbnail from '../../../assets/thumbnail.jpg'
 import RightSideSlider from './Right-side-slider'
 
 const Segment = (props) => {
@@ -9,11 +6,7 @@ const Segment = (props) => {
     <div className={s.Segment}>
       <h3 className={s.SegmentTitle}>{props.title}</h3>
       <div className={s.SegmentParts}>
-        <img src={thumbnail10h} alt='' />
-        <img src={thumbnail10h} alt='' />
-        <img src={thumbnail10h} alt='' />
-        <img src={thumbnail10h} alt='' />
-        <img src={thumbnail10h} alt='' />
+        {props.images.map(image => <img src={image} alt='' />)}
         <button className={s.RightButton}>
           <RightSideSlider />
         </button>
--- src/components/Titles/Titles.js
+++ src/components/Titles/Titles.js
@@ -1,12 +1,37 @@
 import Segment from './Segment/Segment'
 import s from './Titles.module.css'
+import img11 from "../../images/1-1.jpg"
+import img12 from "../../images/1-2.jpg"
+import img13 from "../../images/1-3.jpg"
+import img14 from "../../images/1-4.jpg"
+import img15 from "../../images/1-5.jpg"
+import img21 from "../../images/2-1.jpg"
+import img22 from "../../images/2-2.jpg"
+import img23 from "../../images/2-3.jpg"
+import img24 from "../../images/2-4.jpg"
+import img25 from "../../images/2-5.jpg"
+import img31 from "../../images/3-1.jpg"
+import img32 from "../../images/3-2.jpg"
+import img33 from "../../images/3-3.jpg"
+import img34 from "../../images/3-4.jpg"
+import img35 from "../../images/3-5.jpg"
+import img41 from "../../images/4-1.jpg"
+import img42 from "../../images/4-2.jpg"
+import img43 from "../../images/4-3.jpg"
+import img44 from "../../images/4-4.jpg"
+import img45 from "../../images/4-5.jpg"
 
 const Titles = () => {
+  const segmentProps = {
+    "Netflix'te Popüler": [img11, img12, img13, img14, img15],
+    "Gündemdekiler": [img21, img22, img23, img24, img25],
+    "Gerilim Filmleri": [img31, img32, img33, img34, img35],
+    "Aksiyon ve Macera": [img41, img42, img43, img44, img45]
+  }
 
-  const segmentNames = ["Netflix'te Popüler", "Gündemdekiler", "Gerilim Filmleri", "Aksiyon ve Macera"]
   return (
     <div className={s.Titles}>
-      {segmentNames.map(segmentTitle => <Segment title={segmentTitle} />)}
+      {Object.keys(segmentProps).map(segmentTitle => <Segment key={segmentTitle} title={segmentTitle} images={segmentProps[segmentTitle]} />)}
     </div>
   )
 }
